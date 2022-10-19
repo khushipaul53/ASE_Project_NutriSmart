408aa9bcd4ec18c3e35f21e13f878299eb4ddb78
khushi <you@example.com>
2022-10-19 12:43:55 -0400

    Project added on git

---
+++ app/src/main/java/com/example/ase_project_nutrismart/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ase_project_nutrismart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
